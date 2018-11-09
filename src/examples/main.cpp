6b66e6e0f18665ef58392de722d6796a33fbe256
Steve Downey <sdowney@gmail.com>
2018-11-09 10:41:29 -0500

    Reformat

--- src/examples/main.cpp
+++ src/examples/main.cpp
@@ -13,16 +13,18 @@
 // I prefer to use a namespace alias:
 namespace ranges = std::experimental::ranges;
 
-template<class...> class show_type;
+template <class...>
+class show_type;
 
 void foo(ranges::Readable&) {}
 
-template<typename Maybe,  typename T>
+template <typename Maybe, typename T>
 class maybe_view;
 
-template<typename Maybe,  typename T>
-requires std::is_object_v<T> && std::is_rvalue_reference_v<Maybe>
-class maybe_view<Maybe, T> : public std::experimental::ranges::view_interface<maybe_view<Maybe, T>> {
+template <typename Maybe, typename T>
+requires std::is_object_v<T>&&
+    std::is_rvalue_reference_v<Maybe> class maybe_view<Maybe, T>
+    : public std::experimental::ranges::view_interface<maybe_view<Maybe, T>> {
   private:
     using M = std::remove_cv_t<std::remove_reference_t<Maybe>>;
 
@@ -31,169 +33,213 @@ class maybe_view<Maybe, T> : public std::experimental::ranges::view_interface<ma
   public:
     maybe_view() = default;
 
-    constexpr maybe_view(Maybe maybe) : value_(std::move(maybe))
-    {}
+    constexpr maybe_view(Maybe maybe) : value_(std::move(maybe)) {}
 
-    constexpr T* begin() noexcept { return data(); }
+    constexpr T*       begin() noexcept { return data(); }
     constexpr const T* begin() const noexcept { return data(); }
-    constexpr T* end() noexcept { if (data()) return data() + 1; else return data(); }
-    constexpr const T* end() const noexcept { if (data()) return data() + 1; else return data(); }
-    constexpr std::ptrdiff_t size() noexcept { if (value_.get()) return 1; else return 0;}
+    constexpr T*       end() noexcept {
+        if (data())
+            return data() + 1;
+        else
+            return data();
+    }
+    constexpr const T* end() const noexcept {
+        if (data())
+            return data() + 1;
+        else
+            return data();
+    }
+    constexpr std::ptrdiff_t size() noexcept {
+        if (value_.get())
+            return 1;
+        else
+            return 0;
+    }
 
-    constexpr T* data() noexcept { if (value_.get()) return std::addressof(*value_.get()); else return 0;}
-    constexpr const T* data() const noexcept { if (value_.get()) return std::addressof(*value_.get()); else return 0;}
+    constexpr T* data() noexcept {
+        if (value_.get())
+            return std::addressof(*value_.get());
+        else
+            return 0;
+    }
+    constexpr const T* data() const noexcept {
+        if (value_.get())
+            return std::addressof(*value_.get());
+        else
+            return 0;
+    }
 };
 
-template<typename Maybe,  typename T>
-requires std::is_object_v<T> && std::is_lvalue_reference_v<Maybe>
-class maybe_view<Maybe, T> {
+template <typename Maybe, typename T>
+requires std::is_object_v<T>&&
+    std::is_lvalue_reference_v<Maybe> class maybe_view<Maybe, T> {
     Maybe& value_;
     using R = std::remove_reference_t<decltype(*value_)>;
+
   public:
     maybe_view() = default;
 
-    constexpr maybe_view(Maybe& maybe) : value_(maybe)
-    {}
+    constexpr maybe_view(Maybe& maybe) : value_(maybe) {}
 
-    constexpr R* begin() noexcept { return data(); }
+    constexpr R*       begin() noexcept { return data(); }
     constexpr const R* begin() const noexcept { return data(); }
-    constexpr R* end() noexcept { if (data()) return data() + 1; else return data(); }
-    constexpr const R* end() const noexcept { if (data()) return data() + 1; else return data(); }
-    constexpr std::ptrdiff_t size() noexcept { if (value_.get()) return 1; else return 0;}
+    constexpr R*       end() noexcept {
+        if (data())
+            return data() + 1;
+        else
+            return data();
+    }
+    constexpr const R* end() const noexcept {
+        if (data())
+            return data() + 1;
+        else
+            return data();
+    }
+    constexpr std::ptrdiff_t size() noexcept {
+        if (value_.get())
+            return 1;
+        else
+            return 0;
+    }
 
-    constexpr R* data() noexcept { if (value_) return std::addressof(*value_); else return 0;}
-    constexpr const R* data() const noexcept { if (value_) return std::addressof(*value_); else return 0;}
+    constexpr R* data() noexcept {
+        if (value_)
+            return std::addressof(*value_);
+        else
+            return 0;
+    }
+    constexpr const R* data() const noexcept {
+        if (value_)
+            return std::addressof(*value_);
+        else
+            return 0;
+    }
 };
 
-
-template< class T >
+template <class T>
 struct dereference {
     typedef std::remove_reference_t<decltype(*std::declval<T>())> type;
 };
 
+template <class Maybe>
+maybe_view(const Maybe&)
+    ->maybe_view<const Maybe&, typename dereference<Maybe>::type>;
 
+template <class Maybe>
+maybe_view(Maybe &&)->maybe_view<Maybe&&, typename dereference<Maybe>::type>;
 
-template<class Maybe>
-maybe_view(const Maybe&) -> maybe_view<const Maybe&, typename dereference<Maybe>::type>;
-
-template<class Maybe>
-maybe_view(Maybe&&) -> maybe_view<Maybe&&, typename dereference<Maybe>::type>;
-
-template<class Maybe>
-maybe_view(Maybe&) -> maybe_view<Maybe&, typename dereference<Maybe>::type>;
+template <class Maybe>
+maybe_view(Maybe&)->maybe_view<Maybe&, typename dereference<Maybe>::type>;
 
 // template<class Maybe>
-// maybe_view(Maybe&&) -> maybe_view<Maybe&&, typename dereference<Maybe>::type>;
+// maybe_view(Maybe&&) -> maybe_view<Maybe&&, typename
+// dereference<Maybe>::type>;
 
 // template<class Maybe>
 // maybe_view(const Maybe&) -> maybe_view<Maybe, std::remove_pointer_t<Maybe>>;
 
 // template<class U>
-// explicit maybe_view(U&&) -> maybe_view<invoke_result<decltype(std::ranges::maybe_value), U>>;
+// explicit maybe_view(U&&) ->
+// maybe_view<invoke_result<decltype(std::ranges::maybe_value), U>>;
 
 namespace view {
 struct __maybe_fn {
-    template<class T>
+    template <class T>
     constexpr auto operator()(T&& t) const
-        STL2_NOEXCEPT_REQUIRES_RETURN(
-            maybe_view{std::forward<T>(t)}
-			)
-		};
+        STL2_NOEXCEPT_REQUIRES_RETURN(maybe_view{std::forward<T>(t)})
+};
 
-inline constexpr __maybe_fn maybe {};
-}
+inline constexpr __maybe_fn maybe{};
+} // namespace view
 
 template <typename CHECK>
-void check(CHECK k)
-{
+void check(CHECK k) {
     show_type<CHECK> _;
-
 }
 
-template<typename Maybe>
+template <typename Maybe>
 struct test;
 
-template<typename Maybe>
-requires std::is_rvalue_reference_v<Maybe>
-struct test<Maybe> {
-    test(Maybe) {std::cout << "is_rvalue_reference_v\n";}
+template <typename Maybe>
+requires std::is_rvalue_reference_v<Maybe> struct test<Maybe> {
+    test(Maybe) { std::cout << "is_rvalue_reference_v\n"; }
 };
 
-template<typename Maybe>
-requires std::is_lvalue_reference_v<Maybe>
-struct test<Maybe> {
-    test(Maybe) {std::cout << "is_lvalue_reference_v\n";}
+template <typename Maybe>
+requires std::is_lvalue_reference_v<Maybe> struct test<Maybe> {
+    test(Maybe) { std::cout << "is_lvalue_reference_v\n"; }
 };
 
-template<class Maybe>
-test(const Maybe&) -> test<const Maybe&>;
+template <class Maybe>
+test(const Maybe&)->test<const Maybe&>;
 
-template<class Maybe>
-test(Maybe&&) -> test<Maybe&&>;
+template <class Maybe>
+test(Maybe &&)->test<Maybe&&>;
 
-template<class Maybe>
-test(Maybe&) -> test<Maybe&>;
+template <class Maybe>
+test(Maybe&)->test<Maybe&>;
 
+int  bar() { return 7; }
+int& bar2() {
+    static int i = 9;
+    return i;
+}
 
-int bar(){return 7;}
-int & bar2() {static int i = 9; return i;}
-
-
-class Int
-{
+class Int {
     int i_;
+
   public:
     Int() : i_(0){};
-    explicit  Int(int i) : i_(i){};
-    explicit operator int(){return i_;}
+    explicit Int(int i) : i_(i){};
+    explicit operator int() { return i_; }
 
-    Int operator+(int j) const {return Int(i_ + j);}
+    Int operator+(int j) const { return Int(i_ + j); }
 };
 
-bool operator==(Int lhs, Int rhs){
+bool operator==(Int lhs, Int rhs) {
     return static_cast<int>(lhs) == static_cast<int>(rhs);
 }
 
-bool operator>(Int lhs, Int rhs){
+bool operator>(Int lhs, Int rhs) {
     return static_cast<int>(lhs) > static_cast<int>(rhs);
 }
 
-Int operator+(Int lhs, Int rhs){
+Int operator+(Int lhs, Int rhs) {
     return Int(static_cast<int>(lhs) + static_cast<int>(rhs));
 }
 
-
-class Double
-{
+class Double {
     double d_;
+
   public:
     Double() : d_(0){};
     explicit Double(double d) : d_(d){};
-    explicit operator double(){return d_;}
+    explicit operator double() { return d_; }
 };
 
-bool operator==(Double lhs, Double rhs){
+bool operator==(Double lhs, Double rhs) {
     return static_cast<double>(lhs) == static_cast<double>(rhs);
 };
 
-class NoDefault
-{
+class NoDefault {
   public:
-    NoDefault(int) {};
+    NoDefault(int){};
     NoDefault() = delete;
 };
 
 class NoCopy {
     NoCopy(NoCopy const&) = delete;
+
   public:
-    NoCopy() = default;
+    NoCopy()         = default;
     NoCopy(NoCopy&&) = default;
 };
 
 class NoMove {
     NoMove(NoMove&&) = delete;
+
   public:
-    NoMove() = default;
+    NoMove()              = default;
     NoMove(NoMove const&) = default;
 };
 
@@ -212,10 +258,9 @@ NoCopy makeNoCopy() {
     return noCopy;
 }
 
-
 int main() {
 
-    std::optional s{7};
+    std::optional      s{7};
     std::optional<int> e{};
 
     maybe_view vs2{s};
@@ -229,14 +274,14 @@ int main() {
 
     maybe_view e2{std::optional<int>{}};
     for (int i : e2)
-        std::cout << "i=" <<  i << '\n'; // does not print
+        std::cout << "i=" << i << '\n'; // does not print
 
     auto oe = std::optional<int>{};
     for (int i : view::maybe(oe))
-        std::cout << "i=" <<  i << '\n'; // does not print
+        std::cout << "i=" << i << '\n'; // does not print
 
-    int j = 7;
-    int * pj = &j;
+    int        j  = 7;
+    int*       pj = &j;
     maybe_view vpj{pj};
     for (auto i : vpj)
         std::cout << "i=" << i << '\n'; // prints 7
@@ -246,157 +291,137 @@ int main() {
 
     std::cout << "j=" << j << '\n'; // prints 7
 
-    for (auto&& i : view::maybe(pj))
-    {
+    for (auto&& i : view::maybe(pj)) {
         i = 27;
         std::cout << "i=" << i << '\n'; // prints 7
     }
 
     std::cout << "j=" << j << '\n'; // prints 7
 
-
-    for (auto&& i : view::maybe(s))
-    {
+    for (auto&& i : view::maybe(s)) {
         i = 9;
         std::cout << "i=" << i << '\n'; // prints 7
     }
     std::cout << "s=" << *s << '\n'; // prints 7
 
-    for (auto&& i : std::experimental::ranges::view::single(j))
-    {
+    for (auto&& i : std::experimental::ranges::view::single(j)) {
         i = 19;
         std::cout << "i=" << i << '\n'; // prints 7
     }
     std::cout << "j=" << *s << '\n'; // prints 7
 
-     {
-         auto && __range = view::maybe(s) ;
-         auto __begin = std::begin(__range) ;
-         auto __end = std::end(__range) ;
-         for ( ; __begin != __end; ++__begin) {
-             auto&& i = *__begin;
-             i = 90;
-         }
-     }
-     std::cout << "s=" << *s << '\n'; // prints 7
+    {
+        auto&& __range = view::maybe(s);
+        auto   __begin = std::begin(__range);
+        auto   __end   = std::end(__range);
+        for (; __begin != __end; ++__begin) {
+            auto&& i = *__begin;
+            i        = 90;
+        }
+    }
+    std::cout << "s=" << *s << '\n'; // prints 7
 
-     // std::array<int, 2> a2 = {2,3};
-     // for (auto&& i : view::maybe(a2))
-     // {
-     //     i = 9;
-     //     std::cout << "i=" << i << '\n'; // prints 7
-     // }
+    // std::array<int, 2> a2 = {2,3};
+    // for (auto&& i : view::maybe(a2))
+    // {
+    //     i = 9;
+    //     std::cout << "i=" << i << '\n'; // prints 7
+    // }
 
+    const std::optional      cs{7};
+    const std::optional<int> ce{};
 
-     const std::optional cs{7};
-     const std::optional<int> ce{};
+    maybe_view vcs2{cs};
+    std::cout << *begin(vcs2) << '\n';
 
-     maybe_view vcs2{cs};
-     std::cout << *begin(vcs2) << '\n';
+    for (auto&& i : view::maybe(cs)) {
+        // i = 9;
+        std::cout << "i=" << i << '\n'; // prints 7
+    }
+    std::cout << "cs=" << *cs << '\n'; // prints 7
 
-     for (auto&& i : view::maybe(cs))
-     {
-         // i = 9;
-         std::cout << "i=" << i << '\n'; // prints 7
-     }
-     std::cout << "cs=" << *cs << '\n'; // prints 7
+    std::optional<volatile int> vs{7};
 
+    if (vs) {
+        std::cout << "*vs = " << *vs << '\n';
+    }
+    maybe_view vvs2{vs};
+    std::cout << *begin(vvs2) << '\n';
 
-     std::optional<volatile int> vs{7};
+    for (auto&& i : view::maybe(vs)) {
+        // i = 9;
+        std::cout << "i=" << i << '\n'; // prints 7
+    }
+    std::cout << "vs=" << *s << '\n'; // prints 7
 
-     if (vs) {
-         std::cout << "*vs = " << *vs << '\n';
-     }
-     maybe_view vvs2{vs};
-     std::cout << *begin(vvs2) << '\n';
+    const int          ci  = 11;
+    volatile int       vi  = 12;
+    const volatile int cvi = 13;
 
-     for (auto&& i : view::maybe(vs))
-     {
-         // i = 9;
-         std::cout << "i=" << i << '\n'; // prints 7
-     }
-     std::cout << "vs=" << *s << '\n'; // prints 7
+    auto pci = &ci;
+    for (auto&& i : view::maybe(pci)) {
+        std::cout << "i=" << i << '\n'; // prints 7
+    }
+    std::cout << "pci=" << *pci << '\n'; // prints 7
 
-     const int ci = 11;
-     volatile int vi = 12;
-     const volatile int cvi = 13;
+    auto pvi = &vi;
+    std::cout << "pvi=" << *pvi << '\n'; // prints 7
+    for (auto&& i : view::maybe(pvi)) {
+        i++;
+        std::cout << "i=" << i << '\n'; // prints 7
+    }
+    std::cout << "pvi=" << *pvi << '\n'; // prints 7
 
-     auto pci = &ci;
-     for (auto&& i : view::maybe(pci))
-     {
-         std::cout << "i=" << i << '\n'; // prints 7
-     }
-     std::cout << "pci=" << *pci << '\n'; // prints 7
+    auto pcvi = &cvi;
+    for (auto&& i : view::maybe(pcvi)) {
+        std::cout << "i=" << i << '\n'; // prints 7
+    }
 
-     auto pvi = &vi;
-     std::cout << "pvi=" << *pvi << '\n'; // prints 7
-     for (auto&& i : view::maybe(pvi))
-     {
-         i++;
-         std::cout << "i=" << i << '\n'; // prints 7
-     }
-     std::cout << "pvi=" << *pvi << '\n'; // prints 7
+    int ar[] = {111, 112, 113, 114, 115};
+    for (auto&& i : view::maybe(ar)) {
+        std::cout << "i=" << i << '\n'; // prints 7
+    }
 
-     auto pcvi = &cvi;
-     for (auto&& i : view::maybe(pcvi))
-     {
-         std::cout << "i=" << i << '\n'; // prints 7
-     }
+    Int       myInt{231};
+    Double    myDouble{457.3};
+    NoCopy    noCopy;
+    NoMove    noMove;
+    NoDefault noDefault{678};
 
+    for (auto&& i : view::maybe(std::optional{myInt})) {
+        std::cout << "i=" << int(i) << '\n'; // prints 7
+    }
 
-     int ar[] = {111,112,113,114,115};
-     for (auto&& i : view::maybe(ar))
-     {
-         std::cout << "i=" << i << '\n'; // prints 7
-     }
+    for (auto&& i : view::maybe(std::optional{myDouble})) {
+        std::cout << "i=" << double(i) << '\n'; // prints 7
+    }
 
-     Int myInt{231};
-     Double myDouble{457.3};
-     NoCopy noCopy;
-     NoMove noMove;
-     NoDefault noDefault{678};
+    std::optional<NoCopy> optionalNoCopy;
+    optionalNoCopy.emplace();
+    for (auto&& i : view::maybe(optionalNoCopy)) {
+        std::cout << "No Copy\n";
+    }
 
-     for (auto&& i : view::maybe(std::optional{myInt}))
-     {
-         std::cout << "i=" << int(i) << '\n'; // prints 7
-     }
+    for (auto&& i : view::maybe(std::optional{noMove})) {
+        std::cout << "No Move\n";
+    }
 
-     for (auto&& i : view::maybe(std::optional{myDouble}))
-     {
-         std::cout << "i=" << double(i) << '\n'; // prints 7
-     }
+    for (auto&& i : view::maybe(std::optional{noDefault})) {
+        std::cout << "No Default\n";
+    }
 
-     std::optional<NoCopy> optionalNoCopy;
-     optionalNoCopy.emplace();
-     for (auto&& i : view::maybe(optionalNoCopy))
-     {
-         std::cout << "No Copy\n";
-     }
+    int        kkk    = 7;
+    int&       r_kkk  = kkk;
+    int const& cr_kkk = kkk;
+    int&&      rv_kkk = std::move(kkk);
 
-     for (auto&& i : view::maybe(std::optional{noMove}))
-     {
-         std::cout << "No Move\n";
-     }
-
-     for (auto&& i : view::maybe(std::optional{noDefault}))
-     {
-         std::cout << "No Default\n";
-     }
-
-
-     int kkk = 7;
-     int & r_kkk = kkk;
-     int const& cr_kkk = kkk;
-     int && rv_kkk = std::move(kkk);
-
-     test to1(std::optional<int>{});
-     test to2(std::optional<int>{9});
-     test t0(9);
-     test t1(kkk);
-     test t2(r_kkk);
-     test t3(cr_kkk);
-     test t4(rv_kkk);
-     test t5(bar());
-     test t6(bar2());
-
+    test to1(std::optional<int>{});
+    test to2(std::optional<int>{9});
+    test t0(9);
+    test t1(kkk);
+    test t2(r_kkk);
+    test t3(cr_kkk);
+    test t4(rv_kkk);
+    test t5(bar());
+    test t6(bar2());
 }
