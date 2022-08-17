447d6f293baa2f9c6d8f4600f70e10908af5196a
Steve Downey <sdowney@gmail.com>
2022-08-17 18:04:03 -0400

    Mised some files

---
+++ papers/targets.mk
@@ -0,0 +1,36 @@
+VENV := .venv/
+SOURCE_VENV := . $(VENV)/bin/activate;
+PYEXEC := $(SOURCE_VENV) python
+PIP_SYNC := $(PYEXEC) -m piptools sync
+PYEXECPATH ?= $(shell which python3.9 || which python3.9 || which python3.8 || which python3.7 || which python3)
+PYTHON ?= $(shell basename $(PYEXECPATH))
+REQS_MARKER := $(VENV)/bin/.pip-sync
+PIP := $(PYEXEC) -m pip
+DEPS_DIR := .deps
+
+override DEPS := $(VENV) wg21.bib
+
+
+$(VENV):
+	$(PYTHON) -m venv $(VENV)
+	$(PIP) install setuptools pip pip-tools wheel
+	$(PIP_SYNC) requirements.txt
+
+.PHONY: create-venv
+create-venv:
+	rm -rf $(VENV)
+	make $(VENV)
+
+%.pdf : %.tex wg21.bib | $(VENV)
+	mkdir -p $(DEPS_DIR)
+	$(SOURCE_VENV) latexmk -shell-escape -pdflua -use-make -deps -deps-out=$(DEPS_DIR)/$@.d -MP $<
+
+wg21.bib:
+	curl https://wg21.link/index.bib > wg21.bib
+
+.PHONY: clean
+clean:
+	latexmk -c
+
+# Include dependencies
+$(foreach file,$(TARGET),$(eval -include $(DEPS_DIR)/$(file).d))
---
+++ src/examples/before_after.cpp
@@ -0,0 +1,138 @@
+#include <view_maybe/view_maybe.h>
+#include <view_maybe/view_nullable.h>
+
+#include <vector>
+#include <cassert>
+
+std::optional<int> possible_value() { return {7}; }
+
+void use(int t) { assert(t == t); }
+
+void before0() {
+    {
+        auto&& opt = possible_value();
+        if (opt) {
+            // a few dozen lines ...
+            use(*opt); // is *opt OK ?
+        }
+    }
+}
+
+void after0() {
+    for (auto&& opt : views::nullable(possible_value())) {
+        // a few dozen lines ...
+        use(opt); // opt is OK
+    }
+}
+
+void before1() {
+    std::optional o{7};
+    if (o) {
+        *o = 9;
+        std::cout << "o=" << *o << " prints 9\n";
+    }
+    std::cout << "o=" << *o << " prints 9\n";
+}
+
+void after1() {
+    std::optional o{7};
+    for (auto&& i : views::nullable(std::ref(o))) {
+        i = 9;
+        std::cout << "i=" << i << " prints 9\n";
+    }
+    std::cout << "o=" << *o << " prints 9\n";
+
+    // if range for is too much magic
+    if (auto v = views::nullable(std::ref(o)); std::begin(v) != std::end(v)) {
+        auto itr = std::begin(v);
+        *itr     = 10;
+        std::cout << "*itr=" << *itr << " prints 10\n";
+    }
+    std::cout << "o=" << *o << " prints 10\n";
+}
+
+void before2() {
+    std::vector<int> v{2, 3, 4, 5, 6, 7, 8, 9, 1};
+    auto             test = [](int i) -> std::optional<int> {
+        switch (i) {
+        case 1:
+        case 3:
+        case 7:
+        case 9:
+            return i;
+        default:
+            return {};
+        }
+    };
+
+    auto&& r = v | ranges::views::transform(test) |
+               ranges::views::filter([](auto x) { return bool(x); }) |
+               ranges::views::transform([](auto x) { return *x; }) |
+               ranges::views::transform([](int i) {
+                   std::cout << i;
+                   return i;
+               });
+    for (auto&& i : r) {
+    };
+}
+
+void after2() {
+    std::vector<int> v{2, 3, 4, 5, 6, 7, 8, 9, 1};
+    auto             test = [](int i) -> std::optional<int> {
+        switch (i) {
+        case 1:
+        case 3:
+        case 7:
+        case 9:
+            return i;
+        default:
+            return {};
+        }
+    };
+
+    auto&& r = v | ranges::views::transform(test) |
+               ranges::views::transform(views::nullable) |
+               ranges::views::join | ranges::views::transform([](int i) {
+                   std::cout << i;
+                   return i;
+               });
+    for (auto&& i : r) {
+    };
+    std::cout << std::endl;
+}
+
+void after3() {
+    std::vector<int> v{2, 3, 4, 5, 6, 7, 8, 9, 1};
+    auto             test = [](int i) -> maybe_view<int> {
+        switch (i) {
+        case 1:
+        case 3:
+        case 7:
+        case 9:
+            return maybe_view{i};
+        default:
+            return maybe_view<int>{};
+        }
+    };
+
+    auto&& r = v | ranges::views::transform(test) | ranges::views::join |
+               ranges::views::transform([](int i) {
+                   std::cout << i;
+                   return i;
+               });
+    for (auto&& i : r) {
+    };
+    std::cout << std::endl;
+}
+
+int main() {
+    before0();
+    after0();
+
+    before1();
+    after1();
+
+    before2();
+    after2();
+    after3();
+}
