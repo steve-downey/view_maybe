741ed9837e3e385a03fd83b7963375b5bf0eb33e
Steve Downey <sdowney2@bloomberg.net>
2024-05-20 21:15:49 -0400

    Add reference_or and yield_if
    Impls for reference_or and yield_if

--- src/smd/maybe.h
+++ src/smd/maybe.h
@@ -2,7 +2,6 @@
 #ifndef INCLUDED_SMD_MAYBE
 #define INCLUDED_SMD_MAYBE
 
-
 //@PURPOSE:
 //
 //@CLASSES:
@@ -11,6 +10,9 @@
 //
 //@DESCRIPTION:
 
+#include <optional>
+#include <iterator>
+
 namespace smd {
 template <class T>
 concept maybe = requires(const T t) {
@@ -18,5 +20,35 @@ concept maybe = requires(const T t) {
     *(t);
 };
 
+template <class T>
+constexpr auto yield_if(bool b, T&& t) -> std::optional<std::decay_t<T>>;
+
+template <smd::maybe T, class U, class R>
+auto reference_or(T&& m, U&& u) -> R;
+
+template <smd::maybe T, class U>
+auto value_or();
+
+template <class T, class I>
+auto or_invoke();
+
 } // namespace smd
+
+template <class T>
+constexpr auto smd::yield_if(bool b, T&& t) -> std::optional<std::decay_t<T>> {
+    using O = std::optional<std::decay_t<T>>;
+    return b ? std::forward<T>(t) : O{};
+}
+
+template <smd::maybe T,
+          class U,
+          class R = std::common_reference_t<std::iter_reference_t<T>, U&&>>
+auto smd::reference_or(T&& m, U&& u) -> R {
+#ifdef __cpp_lib_reference_from_temporary
+    static_assert(!std::reference_constructs_from_temporary_v<R, U>);
+    static_assert(!std::reference_constructs_from_temporary_v<R, T&>);
+#endif
+    return bool(m) ? static_cast<R>(*m) : static_cast<R>((U&&)u);
+}
+
 #endif
--- src/smd/maybe.t.cpp
+++ src/smd/maybe.t.cpp
@@ -5,6 +5,7 @@
 
 #include <optional>
 #include <memory>
+#include <array>
 
 using namespace smd;
 
@@ -22,10 +23,88 @@ TEST(MaybeConceptsTest, Concept) {
 
     static_assert(maybe<int*>);
     static_assert(maybe<const int*>);
-    static_assert(maybe<int const *>);
+    static_assert(maybe<const int*>);
 
     static_assert(maybe<decltype(func)>);
 
     static_assert(maybe<std::shared_ptr<int>>);
     static_assert(maybe<std::unique_ptr<int>>);
+}
+
+TEST(MaybeConceptsTest, YieldIf) {
+    bool t = true;
+    bool f = false;
+    int  i = 5;
+
+    auto v1 = smd::yield_if(t, 5);
+    auto v2 = smd::yield_if(t, i);
+
+    auto e1 = smd::yield_if(f, i);
+    auto e2 = smd::yield_if(f, 5);
+
+    ASSERT_EQ(v1, v2);
+    ASSERT_NE(v1, e1);
+    ASSERT_EQ(e1, e2);
+
+    int  a[] = {5};
+    auto a1  = smd::yield_if(t, a);
+
+    std::optional<int*> o1 = std::make_optional(a);
+
+    ASSERT_EQ(a1, a1);
+    ASSERT_EQ(a1, o1);
+
+    std::array<int, 5> ar = {0, 1, 2, 3, 4};
+    auto               a2 = smd::yield_if(t, ar);
+    ASSERT_EQ(a2, a2);
+}
+
+int   ifunc();
+int&& irfunc();
+
+std::string& testLVal(std::string& l) {return l;}
+
+
+TEST(MaybeConceptsTest, ReferenceOr) {
+    std::optional<int> e;
+    std::optional<int> five{5};
+
+    const int fortytwo = 42;
+    int       nine     = 9;
+
+    // auto&& r1 = smd::reference_or(e, nine);
+    // auto&& r2 = smd::reference_or(five, fortytwo);
+    // auto&& r3 = smd::reference_or(five, ifunc());
+
+    static_assert(std::same_as<int&, decltype(smd::reference_or(e, nine))>);
+    static_assert(
+        std::same_as<const int&, decltype(smd::reference_or(five, fortytwo))>);
+    static_assert(
+        std::same_as<const int&, decltype(smd::reference_or(five, ifunc()))>);
+    static_assert(
+        std::same_as<const int&, decltype(smd::reference_or(five, irfunc()))>);
+
+    std::string longString{"A very long string that is not short at all and will allocate"};
+    std::optional<std::string> os{"data"};
+    std::optional<std::string> es{};
+
+    std::string s1 = smd::reference_or(os, testLVal(longString));
+    std::string s2 = smd::reference_or(es, testLVal(longString));
+
+    static_assert(std::same_as<std::string &, decltype(smd::reference_or(os, testLVal(longString)))>);
+
+    std::string& r1 = smd::reference_or(es, testLVal(longString));
+    std::string& r2 = smd::reference_or(os, testLVal(longString));
+
+    ASSERT_EQ(r1, longString);
+    ASSERT_EQ(r2, "data");
+
+    r1 = "assign a string";
+    r2 = "assign a different string";
+
+    ASSERT_EQ(r1, longString);
+    ASSERT_EQ(longString, "assign a string");
+    ASSERT_NE(r2, "data");
+    ASSERT_EQ(os, "assign a different string");
+
 }
